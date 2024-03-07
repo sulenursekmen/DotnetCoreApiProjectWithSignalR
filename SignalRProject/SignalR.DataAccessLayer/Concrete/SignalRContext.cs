4e7bb2dfdfa6fa554f69e3e90f2e434889e03d02
sulenursekmen <sulenursekmenn@gmail.com>
2024-03-07 19:31:29 +0300

    Created project and layer, added entity

---
+++ SignalRProject/SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.Concrete
+{
+    public class SignalRContext : DbContext
+    {
+       
+        public SignalRContext(DbContextOptions options):base(options) 
+        {
+
+        }
+        
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
